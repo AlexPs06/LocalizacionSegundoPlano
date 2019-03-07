6129ba35726057ede742a1338e4c3cf3b9b89b9a
Augusto Rucle <augustorucle@gmail.com>
2019-03-07 12:54:59 -0600

    Se agregaron nuevas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'modal', loadChildren: './components/modal/modal.module#ModalPageModule' },
+  { path: 'inicio', loadChildren: './inicio/inicio.module#InicioPageModule' },  { path: 'visualizador', loadChildren: './visualizador/visualizador.module#VisualizadorPageModule' },
+
+
 ];
 
 @NgModule({
